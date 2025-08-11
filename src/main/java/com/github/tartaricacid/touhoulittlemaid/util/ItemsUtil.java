f33bc55fb5065d30094fcfa61f0d333ab8d8d359
mikasa <1244456115@qq.com>
2025-08-11 13:12:22 +0800

    Sync upstream-1.21.1

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/util/ItemsUtil.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/util/ItemsUtil.java
@@ -2,6 +2,7 @@ package com.github.tartaricacid.touhoulittlemaid.util;
 
 import cn.sh1rocu.touhoulittlemaid.util.itemhandler.IItemHandler;
 import cn.sh1rocu.touhoulittlemaid.util.itemhandler.ItemHandlerHelper;
+import cn.sh1rocu.touhoulittlemaid.util.itemhandler.PlayerMainInvWrapper;
 import com.github.tartaricacid.touhoulittlemaid.api.bauble.IMaidBauble;
 import com.github.tartaricacid.touhoulittlemaid.entity.passive.EntityMaid;
 import com.github.tartaricacid.touhoulittlemaid.inventory.handler.BaubleItemHandler;
@@ -12,6 +13,7 @@ import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
@@ -149,5 +151,30 @@ public final class ItemsUtil {
             ItemEntity itemEntity = new ItemEntity(maid.level(), maid.getX(), maid.getY() + 0.5, maid.getZ(), stack);
             maid.level.addFreshEntity(itemEntity);
         }
+    }
+
+    /**
+     * 判断玩家主背包（包括快捷栏）能否插入物品
+     *
+     * @param player 要检查的玩家
+     * @return 如果背包已满返回true，否则返回false
+     */
+    public static boolean canItemInsert(Player player, ItemStack testStack) {
+        // 获取玩家主背包的物品处理器（与giveItemToPlayer使用相同的包装器）
+        IItemHandler inventory = new PlayerMainInvWrapper(player.getInventory());
+
+        // 遍历所有背包槽位
+        for (int i = 0; i < inventory.getSlots(); i++) {
+            // 模拟插入物品（第三个参数为true表示仅测试，不实际修改物品栏）
+            ItemStack remainder = inventory.insertItem(i, testStack, true);
+
+            // 如果插入后没有剩余，说明该槽位可以容纳物品
+            if (remainder.isEmpty()) {
+                return true;
+            }
+        }
+
+        // 所有槽位都无法容纳测试物品
+        return false;
     }
 }
