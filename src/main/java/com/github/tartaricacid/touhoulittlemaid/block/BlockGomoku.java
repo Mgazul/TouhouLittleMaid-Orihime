6a33c783be37b6a9b0527300f1df32e8a434fbb0
mikasa <1244456115@qq.com>
2025-09-30 11:49:03 +0800

    Sync upstream

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/block/BlockGomoku.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/block/BlockGomoku.java
@@ -14,6 +14,7 @@ import com.github.tartaricacid.touhoulittlemaid.entity.passive.EntityMaid;
 import com.github.tartaricacid.touhoulittlemaid.init.InitItems;
 import com.github.tartaricacid.touhoulittlemaid.init.InitSounds;
 import com.github.tartaricacid.touhoulittlemaid.init.InitTrigger;
+import com.github.tartaricacid.touhoulittlemaid.item.ItemHakureiGohei;
 import com.github.tartaricacid.touhoulittlemaid.network.message.GomokuClientPackage;
 import com.github.tartaricacid.touhoulittlemaid.network.message.SpawnParticlePackage;
 import com.github.tartaricacid.touhoulittlemaid.tileentity.TileEntityGomoku;
@@ -237,13 +238,27 @@ public class BlockGomoku extends BlockJoy implements IBoardGameBlock, IBlock {
 
     @Override
     public ItemInteractionResult useItemOn(ItemStack itemStack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
-        if (level instanceof ServerLevel serverLevel && hand == InteractionHand.MAIN_HAND && player.getMainHandItem().isEmpty()) {
+        if (level instanceof ServerLevel serverLevel && hand == InteractionHand.MAIN_HAND) {
             GomokuPart part = state.getValue(PART);
             BlockPos centerPos = pos.subtract(new Vec3i(part.getPosX(), 0, part.getPosY()));
             BlockEntity te = level.getBlockEntity(centerPos);
             if (!(te instanceof TileEntityGomoku gomoku)) {
                 return ItemInteractionResult.FAIL;
             }
+
+            // 如果是创造模式拿着御币点击，那么铺满棋盘，只剩三个位置
+            if (player.getAbilities().instabuild && itemStack.getItem() instanceof ItemHakureiGohei) {
+                gomoku.clickWithDebug();
+                gomoku.refresh();
+                level.playSound(null, centerPos, InitSounds.GOMOKU_RESET, SoundSource.BLOCKS, 1.0f, 1.0f);
+                return ItemInteractionResult.SUCCESS;
+            }
+
+            // 然后是下棋，必须空手
+            if (!itemStack.isEmpty()) {
+                return ItemInteractionResult.SKIP_DEFAULT_BLOCK_INTERACTION;
+            }
+
             Vec3 location = hit.getLocation().subtract(pos.getX(), pos.getY(), pos.getZ());
             Direction facing = state.getValue(FACING);
             if (isClickChessBox(location.x, location.z, part, facing)) {
@@ -278,7 +293,7 @@ public class BlockGomoku extends BlockJoy implements IBoardGameBlock, IBlock {
                 return ItemInteractionResult.FAIL;
             }
             Point playerPoint = new Point(clickPos[0], clickPos[1], Point.BLACK);
-            if (gomoku.isInProgress() && chessData[playerPoint.x][playerPoint.y] == Point.EMPTY) {
+            if (gomoku.getStatue() == Statue.IN_PROGRESS && chessData[playerPoint.x][playerPoint.y] == Point.EMPTY) {
                 gomoku.setChessData(playerPoint.x, playerPoint.y, playerPoint.type);
                 Statue statue = MaidGomokuAI.getStatue(chessData, playerPoint);
                 // 但是和其他人的女仆对弈不加好感哦
@@ -298,9 +313,9 @@ public class BlockGomoku extends BlockJoy implements IBoardGameBlock, IBlock {
                         InitTrigger.MAID_EVENT.trigger(serverPlayer, TriggerType.WIN_GOMOKU);
                     }
                 }
-                gomoku.setInProgress(statue == Statue.IN_PROGRESS);
+                gomoku.setStatue(statue);
                 level.playSound(null, pos, InitSounds.GOMOKU, SoundSource.BLOCKS, 1.0f, 0.8F + level.random.nextFloat() * 0.4F);
-                if (gomoku.isInProgress() && player instanceof ServerPlayer serverPlayer) {
+                if (gomoku.getStatue() == Statue.IN_PROGRESS && player instanceof ServerPlayer serverPlayer) {
                     gomoku.setPlayerTurn(false);
                     ServerPlayNetworking.send(serverPlayer, new GomokuClientPackage(centerPos, chessData, playerPoint, maid.getGameRecordManager().getGomokuWinCount()));
                 }
--- src/main/java/com/github/tartaricacid/touhoulittlemaid/tileentity/TileEntityGomoku.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/tileentity/TileEntityGomoku.java
@@ -3,6 +3,7 @@ package com.github.tartaricacid.touhoulittlemaid.tileentity;
 import cn.sh1rocu.touhoulittlemaid.api.extension.IBlockEntityPersistentData;
 import com.github.tartaricacid.touhoulittlemaid.api.block.IBoardGameEntityBlock;
 import com.github.tartaricacid.touhoulittlemaid.api.game.gomoku.Point;
+import com.github.tartaricacid.touhoulittlemaid.api.game.gomoku.Statue;
 import com.github.tartaricacid.touhoulittlemaid.init.InitBlocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.HolderLookup;
@@ -10,6 +11,7 @@ import net.minecraft.nbt.ByteArrayTag;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.nbt.Tag;
+import net.minecraft.util.Mth;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
@@ -18,12 +20,13 @@ import java.util.List;
 public class TileEntityGomoku extends TileEntityJoy implements IBoardGameEntityBlock, IBlockEntityPersistentData {
     public static final BlockEntityType<TileEntityGomoku> TYPE = BlockEntityType.Builder.of(TileEntityGomoku::new, InitBlocks.GOMOKU).build(null);
     private static final String CHESS_DATA = "ChessData";
-    private static final String IN_PROGRESS = "InProgress";
+    private static final String STATUE = "Statue";
     private static final String PLAYER_TURN = "PlayerTurn";
     private static final String CHESS_COUNTER = "ChessCounter";
     private static final String LATEST_CHESS_POINT = "LatestChessPoint";
+
     private byte[][] chessData = new byte[15][15];
-    private boolean inProgress = true;
+    private int statue = Statue.IN_PROGRESS.ordinal();
     private boolean playerTurn = true;
     private int chessCounter = 0;
     private Point latestChessPoint = Point.NULL;
@@ -39,7 +42,7 @@ public class TileEntityGomoku extends TileEntityJoy implements IBoardGameEntityB
             listTag.add(new ByteArrayTag(chessRow));
         }
         tlm$getPersistentData().put(CHESS_DATA, listTag);
-        tlm$getPersistentData().putBoolean(IN_PROGRESS, this.inProgress);
+        tlm$getPersistentData().putInt(STATUE, this.statue);
         tlm$getPersistentData().putBoolean(PLAYER_TURN, this.playerTurn);
         tlm$getPersistentData().putInt(CHESS_COUNTER, this.chessCounter);
         tlm$getPersistentData().put(LATEST_CHESS_POINT, Point.toTag(this.latestChessPoint));
@@ -54,7 +57,7 @@ public class TileEntityGomoku extends TileEntityJoy implements IBoardGameEntityB
             ByteArrayTag byteArray = (ByteArrayTag) listTag.get(i);
             this.chessData[i] = byteArray.getAsByteArray();
         }
-        this.inProgress = tlm$getPersistentData().getBoolean(IN_PROGRESS);
+        this.statue = tlm$getPersistentData().getInt(STATUE);
         this.playerTurn = tlm$getPersistentData().getBoolean(PLAYER_TURN);
         this.chessCounter = tlm$getPersistentData().getInt(CHESS_COUNTER);
         this.latestChessPoint = Point.fromTag(tlm$getPersistentData().getCompound(LATEST_CHESS_POINT));
@@ -62,7 +65,7 @@ public class TileEntityGomoku extends TileEntityJoy implements IBoardGameEntityB
 
     public void reset() {
         this.chessData = new byte[15][15];
-        this.inProgress = true;
+        this.statue = Statue.IN_PROGRESS.ordinal();
         this.playerTurn = true;
         this.chessCounter = 0;
         this.latestChessPoint = Point.NULL;
@@ -84,6 +87,32 @@ public class TileEntityGomoku extends TileEntityJoy implements IBoardGameEntityB
         this.chessCounter += 1;
     }
 
+    // 调试功能，铺满棋盘，只剩三个位置
+    public void clickWithDebug() {
+        byte[][] drawBoard = new byte[15][15];
+        for (int x = 0; x < 15; x++) {
+            boolean blackFirst = (x / 2) % 2 == 0;
+            for (int y = 0; y < 15; y++) {
+                // 14,(12-14) 留空
+                if (x == 14 && 12 <= y) {
+                    drawBoard[x][y] = Point.EMPTY;
+                    continue;
+                }
+                // 奇偶交替填充，避免出现五连
+                if (blackFirst) {
+                    drawBoard[x][y] = (y % 2 == 0) ? (byte) Point.BLACK : (byte) Point.WHITE;
+                } else {
+                    drawBoard[x][y] = (y % 2 == 0) ? (byte) Point.WHITE : (byte) Point.BLACK;
+                }
+            }
+        }
+        this.chessData = drawBoard;
+        this.latestChessPoint = new Point(14, 10, Point.WHITE);
+        this.chessCounter = 15 * 15 - 3;
+        this.statue = Statue.IN_PROGRESS.ordinal();
+        this.playerTurn = true;
+    }
+
     public boolean isPlayerTurn() {
         return playerTurn;
     }
@@ -92,12 +121,12 @@ public class TileEntityGomoku extends TileEntityJoy implements IBoardGameEntityB
         this.playerTurn = playerTurn;
     }
 
-    public boolean isInProgress() {
-        return inProgress;
+    public void setStatue(Statue statue) {
+        this.statue = statue.ordinal();
     }
 
-    public void setInProgress(boolean inProgress) {
-        this.inProgress = inProgress;
+    public Statue getStatue() {
+        return Statue.values()[Mth.clamp(statue, 0, Statue.values().length - 1)];
     }
 
     public int getChessCounter() {
